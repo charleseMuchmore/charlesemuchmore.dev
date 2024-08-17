66017785f850885c37c07acce228577c32050f1b
Charlese Muchmore <charlymuchmore@gmail.com>
2024-08-17 10:00:55 -0700

    Added a basic temporary landing page.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>
+          Hi there, looks like you've found my website!
+        </h2>
+        <p>My name is Charlese, and I am a web developer.</p>
+        <p>This site is currently under construction...</p>
+        <p>Check back later!</p>
       </header>
     </div>
   );
