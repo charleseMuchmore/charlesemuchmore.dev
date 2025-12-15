fa65dc3e760f4d61eb1c25243a0589dc65b78b04
charleseMuchmore <87589934+charleseMuchmore@users.noreply.github.com>
2025-12-15 15:05:47 -0800

    Added further setup to API.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
+const cors = require('cors'):
 const port = 3001;
+const domain = "https://charlesemuchmore.dev";
+
+const app = express();
+app.use(cors({
+  origin: domain
+}));
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend!');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`App is running on port ${port}`);
 });
